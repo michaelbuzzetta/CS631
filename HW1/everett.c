db574c61fcfce5f09777a23113161e7584f785db
Michael Buzzetta <michaelbuzzetta@mac.mynetworksettings.com>
2026-09-22 13:42:09 -0400

    n flag implimented

--- HW1/everett.c
+++ HW1/everett.c
@@ -2,24 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int option;
     char *word = "World";
     int count = 1;
-    
-    while ((option = getopt(argc, argv, "hn:w:")) !=-1) {
-        switch (option) {
+
+    while ((option = getopt(argc, argv, "hn:w:")) != -1)
+    {
+        switch (option)
+        {
         case 'h':
             printf("usage: everett [-h] [-n num] [-w word]\n");
             break;
         case 'w':
             word = optarg;
             break;
+        case 'n':
+            count = atoi(optarg);
+            break;
         default:
             return EXIT_FAILURE;
         }
-        
+    }
+
+    printf("Hеllo,");
+
+    for (int i = 0; i < count; i++)
+    {
+        printf(" %s", word);
+    }
+    printf("\n");
+
     return EXIT_SUCCESS;
 }
