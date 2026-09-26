418abba62236c603170fd46f41233fe41c5b6039
Michael Buzzetta <michaelbuzzetta@mac.mynetworksettings.com>
2026-09-26 11:51:15 -0400

    source error check

--- HW2/bbcp.c
+++ HW2/bbcp.c
@@ -1,13 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
+    int source;
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s source target\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+    source=open(argv[1], O_RDONLY);
+    if(source==-1){
+        perror(argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    if(close(source)==-1){
+        perror(argv[1]);
+        return EXIT_FAILURE;
+    }
 
 	return EXIT_SUCCESS;
 }
